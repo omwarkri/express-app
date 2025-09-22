a6b693b68373568ec21659a3ea048d5f06542d65
omwarkri <owarkri710@gmail.com>
2025-09-22 21:53:13 +0530

    add some world

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Home route
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Express.js which i work , my name is devops and cloud learner today is monday!");
+  res.send("🚀 Hello from Express.js this is a om!");
   
 });
 
